008641ce74d504fe36c90f74a1f202bd8df257ce
Paul Pichon F <paul10_barca@hotmail.com>
2022-10-26 20:06:30 -0500

    mostrando las recetas en el html

--- js/app.js
+++ js/app.js
@@ -7,6 +7,9 @@ function iniciarApp() {
     //agregar un lsitener a selectCategorias para cuando hagan un change
     selectCategorias.addEventListener('change', seleccionarCategoria);
 
+    //variable donde se renderizaran los resultados
+    const resultado = document.querySelector('#resultado');
+
     //llamar la funcion para obtener categorias
     obtenerCategorias();
 
@@ -48,7 +51,69 @@ function iniciarApp() {
         //fetch a la url
         fetch( url )
             .then( respuesta => respuesta.json() )
-            .then( datos => console.log( datos ) );
+            //accedemos a meals
+            //llamamos a la funcion mostrarRecetas
+            .then( datos => mostrarRecetas( datos.meals ) );
+    }
+    //funcion para mostrar recetas en base a la categoria seleccionada
+    function mostrarRecetas( recetas = []) {
+        //iterar en los resultados
+        recetas.forEach( receta => {
+            //destructuring
+            const { idMeal, strMeal, strMealThumb } = receta;
+            //construir el html
+            const recetaContenedor = document.createElement('DIV');
+            //estilos
+            recetaContenedor.classList.add('col-md-4');
+            //card
+            const recetaCard = document.createElement('DIV');
+            //estilos
+            recetaCard.classList.add('card', 'mb-4');
+            //imagen
+            const recetaImagen = document.createElement('IMG');
+            //estilos
+            recetaImagen.classList.add('card-img-top');
+            //alt a la imagen
+            recetaImagen.alt = `Imagen de la receta ${ strMeal }`;
+            //source de la imagen
+            recetaImagen.src = strMealThumb;
+
+            //body del card
+            const recetaCardBody = document.createElement('DIV');
+            //estilos
+            recetaCardBody.classList.add('card-body');
+
+            //heading
+            const recetaHeading = document.createElement('H3');
+            //estilos
+            recetaHeading.classList.add('card-title', 'mb-3');
+            //textcontent
+            recetaHeading.textContent = strMeal;
+
+            //button de enlace
+            const recetaButton = document.createElement('BUTTON');
+            //estilos
+            recetaButton.classList.add('btn', 'btn-danger', 'w-100');
+            recetaButton.textContent = 'Ver Receta';
+
+            //inyectar en el html
+            //heading
+            recetaCardBody.appendChild( recetaHeading );
+            //button
+            recetaCardBody.appendChild( recetaButton );
+            
+            //receta card
+            //imagen
+            recetaCard.appendChild( recetaImagen );
+            //cardbody
+            recetaCard.appendChild( recetaCardBody );
+
+            //card completo
+            recetaContenedor.appendChild( recetaCard );
+
+            //renderizar
+            resultado.appendChild( recetaContenedor );
+        });
     }
 
 }
